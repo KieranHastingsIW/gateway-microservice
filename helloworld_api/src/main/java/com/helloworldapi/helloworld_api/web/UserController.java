f86cca7bc793a5bba33d105fb45d3dd59fe81ad6
KieranHastingsIW <kieran.hastings@integration.works>
2022-12-16 16:04:03 +1300

    omg it maybe could hav worked like next timme

--- helloworld_api/src/main/java/com/helloworldapi/helloworld_api/web/UserController.java
+++ helloworld_api/src/main/java/com/helloworldapi/helloworld_api/web/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RestController;
 
 import com.helloworldapi.helloworld_api.Entity.User;
 import com.helloworldapi.helloworld_api.service.UserService;
@@ -17,7 +17,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-@Controller
+@RestController
 public class UserController {
     
     UserService userService;
